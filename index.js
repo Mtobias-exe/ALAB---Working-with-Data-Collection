75c468a9bd8d8944b7e4e5b26ee18b5293d57d35
Mtobias-exe <maryfrance.tobias@gmail.com>
2024-11-18 16:19:35 -0500

    working on part 2

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-const data ="Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232"
-let cell = [];
-let row = [];
-for (let i = 0; i < data.length; i++){
-    if(data[i] === ","){
-    console.log(cell[i])
-    } else if (data[i] === "\n"){
-        data.push[row]
-    }
+// R-LAB
+// Part 1: Refactoring Old Code
+
+// Part 3: Feeling Loopy
+
+// const data ="Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232"
+// let rows = data.split('\n');
+
+
+// console.log(rows)
+
+
+// Part 2: Expanding Functionality
+
+const data = 'ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26';
+
+
+let arrData = []; 
+let rows = data.split('\n'); 
+let headers = rows[0].split(','); 
+
+// iterates each row excluding the header
+for (let i = 1; i < rows.length; i++) {  
+    let rowData = rows[i].split(',');  
+    arrData[i - 1] = {}; 
+
+    // iterates over the headers array
+    for (let j = 0; j < headers.length; j++) {
+        console.log(headers[j]); 
+        console.log(rowData[j]); 
+
         
+    }
 }
+
+console.log(arrData);
